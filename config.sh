7d79f1e6039d4528947ee552dd554ce051fd5737
hcley <cleysonb.oliveira@gmail.com>
2021-10-25 01:04:06 -0300

    Moving bash variables to config file

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+supernode_ip="192.168.100.5"
+supernode_port="9050"
+
+localport=9040
+
+args="$supernode_ip:$supernode_port super $localport"
--- run.sh
+++ run.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
-
-supernode_ip="192.168.100.5"
-supernode_port="9050"
-
-localport=9040
-
-
-
-
-
-args="$supernode_ip:$supernode_port super $localport"
-
+source config.sh
 
 if [ ! -d "./_class" ]
 then
